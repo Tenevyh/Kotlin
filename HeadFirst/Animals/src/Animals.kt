6424ce102c94aa6a465f6c0a93351f9d3da13d85
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-01-02 02:02:07 +0300

    Update Animals.kt

--- HeadFirst/Animals/src/Animals.kt
+++ HeadFirst/Animals/src/Animals.kt
@@ -54,3 +54,23 @@ open class Wolf:Animal(){
         println("The Wolf is eating $food")
     }
 }
+
+class Vet {
+    fun giveShot (animal: Animal){
+        animal.makeNoise()
+    }
+}
+
+fun main (args: Array<String>) {
+    val animals = arrayOf(Hippo(); Wolf())
+    for (item in animals) {
+        item.roam()
+        item.eat()
+    }
+
+    val vet = Vet()
+    val wolf = Wolf()
+    val Hippo = Hippo()
+    vet.giveShot(wolf)
+    vet.giveShot(hippo)
+}
