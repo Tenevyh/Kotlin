a480cb91347682600de3957e0b0f48fde6c7bcb1
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-01-06 13:18:46 +0300

    Update Animals.kt

--- HeadFirst/Animals/src/Animals.kt
+++ HeadFirst/Animals/src/Animals.kt
@@ -79,4 +79,12 @@ fun main (args: Array<String>) {
     val hippo = Hippo()
     vet.giveShot(wolf)
     vet.giveShot(hippo)
+
+    val roamables = arrayOf(Hippo(), Wolf(), Vehicle())
+    for (item in roamables){
+        item.roam()
+        if (item is Animal) {
+            item.eat()
+        }
+    }
 }
