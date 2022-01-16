25afdb541fc062637d23d6ab81eca1e358ffd83f
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-01-16 19:17:31 +0300

    update project Lambdas

--- HeadFirst/Lambdas/src/Lambdas.kt
+++ HeadFirst/Lambdas/src/Lambdas.kt
@@ -1,17 +1,39 @@
+typealias DoubleConversion = (Double) -> Double
+
+fun convert(x : Double, converter : DoubleConversion) : Double {
+    val result = converter(x)
+    println("$x is converted to $result")
+    return result
+}
+
+fun getConversionLambda(str : String) : DoubleConversion {
+    if (str == "CentigradeToFahrenheit"){
+        return {it * 1.8 + 32}
+    } else if (str == "KgsToPounds"){
+        return {it * 2.204623}
+    } else if (str == "PoundsToUSTons"){
+        return {it / 2000.0}
+    } else {
+        return {it}
+    }
+}
+
+fun combine(lambda1 : DoubleConversion, lambda2: DoubleConversion) : DoubleConversion {
+    return {x : Double -> lambda2(lambda1(x))}
+}
+
 fun main(args : Array<String>){
-    var addFive = {x : Int -> x + 5}
-    println("Pass 6 to addFive : ${addFive(6)}")
+// Преобразование в кг в фунты
+    println("Convert 2.5kg to Pounds : ${getConversionLambda("KgsToPounds")(2.5)}")
 
-    val addInts = {x : Int, y : Int -> x + y}
-    val result = addInts.invoke(6, 7)
-    println("Pass 6, 7 to addInts : ${result}")
+// Определить для лямбда-выражения для преобразования
+    val kgsToPoundsLambda = getConversionLambda("KgsToPounds")
+    val poundsToUSTonsLambda = getConversionLambda("PoundsToUSTons")
 
-    val intLambda : (Int, Int) -> Int = {x, y ->x * y}
-    println("Pass 10, 11 to intLambda : ${intLambda(10, 11)}")
+// Два лямбда-выражения преобразуется в одно новое
+    val kgsToUSTonsLambda = combine(kgsToPoundsLambda, poundsToUSTonsLambda)
 
-    val addSeven : (Int) -> Int = {it + 7}
-    println("Pass 12 to addSeven : ${addSeven(12)}")
-
-    val myLambda : () -> Unit = {println("Hi!")}
-    myLambda()
+// Использовать новое лямбда-выражение для преобразования 17.14 кг в америкакские тонны
+    val value = 17.4
+    println("$value kgs is ${convert(value, kgsToUSTonsLambda)} US tons")
 }
